f6efd9776da3ce70d89a50716df64090f0167d6f
Faizal Heryanto <faizalheryanto23400@gmail.com>
2024-06-07 17:31:05 +0700

    commit narrow n wide type casting

---
+++ Faizal Nurrahman Heryanto/4_Type_Casting/WideningTypeCasting.java
@@ -0,0 +1,14 @@
+// package com.tutorialspoint;
+
+public class WideningTypeCasting {
+    // Main driver method
+
+    public static void main(String[] args) {
+        // Define int variables
+        int num1 = 5004;
+        double num2 = 2.5;
+        double sum = num1 + num2;
+        // show output
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
