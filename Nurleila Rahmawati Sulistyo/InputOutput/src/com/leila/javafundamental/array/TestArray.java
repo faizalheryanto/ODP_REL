b3c89fd53c73f1aedd571202397fb83f5568c575
leilanrs <rnurleila@gmail.com>
2024-06-11 16:32:43 +0700

    array

---
+++ Nurleila Rahmawati Sulistyo/InputOutput/src/com/leila/javafundamental/array/TestArray.java
@@ -0,0 +1,21 @@
+package com.leila.javafundamental.array;
+
+public class TestArray {
+    public static void main(String[] args) {
+        int[] arrInt = new int[]{1, 2, 3, 4, 5, 6};
+        System.out.println(arrInt[0]); // 1
+        System.out.println(arrInt[1]); // 2
+
+        int[] arrInt2 = new int[6];
+        arrInt2[0] = 1;
+        arrInt2[1] = 2;
+
+        System.out.println(arrInt2[0]); //1
+        System.out.println(arrInt2[1]); //2
+
+        int[] arrInt3 = {1, 2, 3, 4, 5, 6};
+        System.out.println(arrInt3[0]); //1
+
+
+    }
+}
