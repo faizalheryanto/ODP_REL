99342a5649b688cc5abc8a46bdbd64bb01c170db
StepByStepen <159742547+StepByStepen@users.noreply.github.com>
2024-06-19 10:00:47 +0700

    Update

---
+++ Steven Lufthanza Bong/JavaTutorial/src/TypeCasting/WideningTypeCasting.java
@@ -0,0 +1,11 @@
+package TypeCasting;
+
+public class WideningTypeCasting {
+    public static void main(String[] args) {
+        int num1 = 5004;
+        double num2 = 2.5;
+        double sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
---
+++ Steven Lufthanza Bong/JavaTutorial/src/TypeCasting/WideningTypeCastingB.java
@@ -0,0 +1,12 @@
+package TypeCasting;
+
+public class WideningTypeCastingB {
+    public static void main(String[] args) {
+
+        int num1 = 5004;
+        double num2 = 2.5;
+        double sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
