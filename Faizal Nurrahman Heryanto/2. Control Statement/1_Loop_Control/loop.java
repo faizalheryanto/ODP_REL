422f9e9e96e915672e71519ac847d1a8ddd5b438
Faizal Heryanto <faizalheryanto23400@gmail.com>
2024-06-19 15:32:53 +0700

    Modul 2 Loop and Decision Making

--- Faizal Nurrahman Heryanto/2. Control Statement/1_Loop_Control/loop.java
+++ Faizal Nurrahman Heryanto/2. Control Statement/1_Loop_Control/loop.java
@@ -116,17 +116,17 @@ public class loop {
         System.out.print("\n<-----END WHILE LOOP----->\n");
 
         System.out.print("\n<-----DO WHILE LOOP----->\n");
-       
+
         System.out.print("\nExample 1: Printing Numbers in a Range Using do while \n");
         int x2 = 10;
         do {
-           System.out.print("value of x : " + x2 );
-           x2++;
-           System.out.print("\n");
-        }while( x2 < 20 );
+            System.out.print("value of x : " + x2);
+            x2++;
+            System.out.print("\n");
+        } while (x2 < 20);
 
         System.out.print("\nExample 2: Printing Elements of an Array Using do while\n");
-        
+
         int[] numbers = {10, 20, 30, 40, 50};
         int index1 = 0;
 
@@ -136,12 +136,96 @@ public class loop {
             System.out.print("\n");
         } while (index1 < 5);
 
-      
+        System.out.print("\n Example 3 : Implementing an infinite do while Loop\n");
+        // int x3 = 10;
 
+        // do {
+        //    System.out.print("value of x : " + x3 );
+        //    x3++;
+        //    System.out.print("\n");
+        // } while( true );
         System.out.print("\n<-----END DO WHILE LOOP----->\n");
 
-        System.out.print("\n<-----IF ELSE----->\n");
-        System.out.print("\n<-----END IF ELSE----->\n");
+        System.out.print("\n<-----BREAK STATEMENT----->\n");
+
+        System.out.print("\nExample 1: Using break with while loop\n");
+        int x4 = 10;
+
+        while (x4 < 20) {
+            if (x4 == 15) {
+                break;
+            }
+            System.out.print("value of x : " + x4);
+            x4++;
+            System.out.print("\n");
+        }
+
+        System.out.print("\nExample 2: Using break with for loop\n");
+
+        int[] numbers4 = {10, 20, 30, 40, 50};
+
+        for (int index2 = 0; index2 < numbers4.length; index2++) {
+            if (numbers4[index2] == 30) {
+                break;
+            }
+            System.out.print("value of item : " + numbers4[index2]);
+            System.out.print("\n");
+        }
+
+        System.out.print("\nExample 3: Using break with an infinite loop\n");
+
+        int x5 = 10;
+
+        while (true) {
+            System.out.print("value of x : " + x5);
+            x5++;
+            if (x5 == 15) {
+                break;
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n<-----END BREAK----->\n");
+
+        System.out.print("\n<-----CONTINUE----->\n");
+
+        System.out.print("\nExample 1: Using continue with while loop\n");
+        int x6 = 10;
+
+        while (x6 < 20) {
+            x6++;
+            if (x6 == 15) {
+                continue;
+            }
+            System.out.print("value of x : " + x6);
+            System.out.print("\n");
+        }
+        System.out.print("\nExample 2: Using continue with for loop\n");
+        int[] numbers5 = {10, 20, 30, 40, 50};
+
+        for (int index3 = 0; index3 < numbers5.length; index3++) {
+            if (numbers5[index3] == 30) {
+                continue;
+            }
+            System.out.print("value of item : " + numbers5[index3]);
+            System.out.print("\n");
+        }
+
+        System.out.print("\nExample 3: Using continue with do while loop\n");
+        int x7 = 10;
+
+        do {
+            x7++;
+            if (x7 == 15) {
+                continue;
+            }
+            System.out.print("value of x : " + x7);
+            System.out.print("\n");
+        } while (x7 < 20);
+
+        System.out.print("\n<-----END CONTINUE----->\n");
+
+
+        
 
     }
 
